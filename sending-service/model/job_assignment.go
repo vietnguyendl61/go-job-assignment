a3cfb4118caad4ac53446777fa12c17a6ce311d1
viet <vietnguyendl61@gmail.com>
2024-02-22 21:30:12 +0700

    add field job_status to table job_assignment

--- sending-service/model/job_assignment.go
+++ sending-service/model/job_assignment.go
@@ -10,4 +10,5 @@ type JobAssignment struct {
 	BookDate    time.Time `json:"book_date" gorm:"column:book_date;not null"`
 	CustomerId  uuid.UUID `json:"customer_id" gorm:"column:customer_id;type:uuid;not null"`
 	Description string    `json:"description" gorm:"column:description;type:text"`
+	JobStatus   string    `json:"status" gorm:"column:status;type:text;not null"`
 }
