5163c42f4d6a4abe7f7c2d04ad27337dec873d07
viet <vietnguyendl61@gmail.com>
2024-02-22 22:18:28 +0700

    check and fix code in booking service

--- booking-service/model/job.go
+++ booking-service/model/job.go
@@ -1,13 +1,11 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"time"
 )
 
 type Job struct {
 	BaseModel
 	BookDate    time.Time `json:"book_date" gorm:"column:book_date;not null"`
-	CustomerId  uuid.UUID `json:"customer_id" gorm:"column:customer_id;type:uuid;not null"`
 	Description string    `json:"description" gorm:"column:description;type:text"`
 }
